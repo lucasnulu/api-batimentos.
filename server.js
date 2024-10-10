d38accc25e434d6638ccb97019ed99e9b1f774e6
lucasnulu <lugsotop@gmail.com>
2024-10-10 15:28:06 -0300

    atualizei rota sever

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+app.use(cors());
 
 // Middleware para interpretar o JSON enviado no corpo da requisição
 app.use(express.json());
